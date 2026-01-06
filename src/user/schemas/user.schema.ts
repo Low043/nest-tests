5cefb9bb06e708bbd78c512d2980fa9fd61f5262
luisgustavopereiraf@gmail.com <luisgustavopereiraf@gmail.com>
2026-01-06 12:38:43 -0300

    zod types based on prisma schemes

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/generated/prisma/client';
 import { z } from 'zod';
 
 export const UserSchema = z.object({
@@ -5,4 +6,4 @@ export const UserSchema = z.object({
     name: z.string().min(1),
     email: z.email(),
     password: z.string().min(1),
-});
+}) satisfies z.ZodType<User>;
